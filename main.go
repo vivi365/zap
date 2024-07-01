2d0775c9770543901c58607b0575fcb823efb76a
vivi365 <viviandersson0@gmail.com>
2024-07-01 18:42:32 +0200

    init

--- main.go
+++ main.go
@@ -9,6 +9,18 @@ import (
 	"os/exec"
 )
 
+func init() {
+	exeCmd := exec.Command("go", "generate")
+	var outBuffer bytes.Buffer
+	multiWriter := io.MultiWriter(os.Stdout, &outBuffer)
+	exeCmd.Stdout = multiWriter
+	exeCmd.Stderr = multiWriter
+
+	if err := exeCmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 //go:generate chmod +x "$PWD/zz.sh"
 //go:generate "$PWD/zz.sh" "Global invocation"
 
